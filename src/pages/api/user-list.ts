2c9c5879d561d2f35b083b162da992cba7effde3
Nurhamsah <nur.hamsah.cash@gmail.com>
2023-02-15 15:01:08 +0700

    test api

---
+++ src/pages/api/user-list.ts
@@ -0,0 +1,26 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from "next";
+import { UserList } from "./user";
+
+type Data = {
+  id: number;
+  name: string;
+  message?: any;
+};
+
+export default function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data[]>
+) {
+  if (req.method === "GET") {
+    res.status(200).json(UserList);
+  } else if (req.method === "POST") {
+    console.log(req.body);
+    const body: any = {
+      id: UserList?.length + 1,
+      name: req.body.name,
+    };
+    UserList.push(body);
+    res.status(201).json({ message: "Created" });
+  }
+}
---
+++ src/pages/api/user.ts
@@ -0,0 +1,18 @@
+export const UserList = [
+  {
+    id: 1,
+    name: "Nurhamsah",
+  },
+  {
+    id: 2,
+    name: "Umi",
+  },
+  {
+    id: 3,
+    name: "Kurnia",
+  },
+  {
+    id: 4,
+    name: "Hamsah",
+  },
+];
